01c533b2ab6e24ff82811d97706b9fa0b7bae19d
ArshpreetSinghGill <162580817+ArshpreetSinghGill@users.noreply.github.com>
2024-10-25 11:35:12 +0530

    Created BigPig.java
    Create BigPig class extending Pig with inherited attributes and constructor

---
+++ core/src/main/java/com/AngryHomies/AngryBirds/BigPig.java
@@ -0,0 +1,9 @@
+package com.AngryHomies.AngryBirds;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class BigPig extends Pig {
+    public BigPig(Texture texture, float x, float y, float width, float height) {
+        super(texture, x, y, width, height);
+    }
+}
