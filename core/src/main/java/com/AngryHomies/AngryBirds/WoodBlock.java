f2c4e0677cfd67764215ce824d6090048479e14a
kushiiitd05 <162983932+kushiiitd05@users.noreply.github.com>
2024-10-25 11:47:32 +0530

    Create WoodBlock.java
    here is anothe  child of the parent Block to be distinguished  for future ...

---
+++ core/src/main/java/com/AngryHomies/AngryBirds/WoodBlock.java
@@ -0,0 +1,9 @@
+package com.AngryHomies.AngryBirds;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class WoodBlock extends Block {
+    public WoodBlock(Texture texture, float x, float y, float width, float height) {
+        super(texture, x, y, width, height);
+    }
+}
