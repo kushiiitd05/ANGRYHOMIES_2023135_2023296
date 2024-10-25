4f88be109321d47d22d5fc4f7089fd81888056d6
kushiiitd05 <162983932+kushiiitd05@users.noreply.github.com>
2024-10-25 11:45:21 +0530

    Create GlassBlock.java
    this is the child of the Block class having its unique feature to be implemented in the future

---
+++ core/src/main/java/com/AngryHomies/AngryBirds/GlassBlock.java
@@ -0,0 +1,9 @@
+package com.AngryHomies.AngryBirds;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class GlassBlock extends Block {
+    public GlassBlock(Texture texture, float x, float y, float width, float height) {
+        super(texture, x, y, width, height);
+    }
+}
