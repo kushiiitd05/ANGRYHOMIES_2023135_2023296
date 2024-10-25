ec2319e04eda588e016bd230e00daa964248f053
kushiiitd05 <162983932+kushiiitd05@users.noreply.github.com>
2024-10-25 11:48:51 +0530

    Create StoneBlock.java
    other of third of  child of Block class

---
+++ core/src/main/java/com/AngryHomies/AngryBirds/StoneBlock.java
@@ -0,0 +1,9 @@
+package com.AngryHomies.AngryBirds;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class StoneBlock extends Block {
+    public StoneBlock(Texture texture, float x, float y, float width, float height) {
+        super(texture, x, y, width, height);
+    }
+}
