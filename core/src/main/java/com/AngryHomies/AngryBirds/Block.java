b7baa6acb66625a261b9d29d8441443f49f68704
kushiiitd05 <162983932+kushiiitd05@users.noreply.github.com>
2024-10-25 11:41:56 +0530

    Create Block.java
    i ve created  a block class   here implemented its cordinate AND THE HT AND WIDTH  as well as the texture

---
+++ core/src/main/java/com/AngryHomies/AngryBirds/Block.java
@@ -0,0 +1,46 @@
+package com.AngryHomies.AngryBirds;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public abstract class Block {
+    protected Texture texture;
+    protected float x, y, width, height;
+
+    public Block(Texture texture, float x, float y, float width, float height) {
+        this.texture = texture;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public Texture getTexture() {
+        return texture;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+}
+
+
+
+
+
+
+
+
+
+
