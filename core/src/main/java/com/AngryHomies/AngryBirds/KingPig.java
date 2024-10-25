8284f19a2230ed5a6650b84f30d675159f118914
ArshpreetSinghGill <162580817+ArshpreetSinghGill@users.noreply.github.com>
2024-10-25 11:36:38 +0530

    Created KingPig.java
    Add KingPig class extending Pig with inherited attributes and constructor

---
+++ core/src/main/java/com/AngryHomies/AngryBirds/KingPig.java
@@ -0,0 +1,9 @@
+package com.AngryHomies.AngryBirds;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class KingPig extends Pig {
+    public KingPig(Texture texture, float x, float y, float width, float height) {
+        super(texture, x, y, width, height);
+    }
+}
