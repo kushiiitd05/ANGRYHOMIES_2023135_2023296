2f11ecdb9c088bc7e7a6a54b4194c3b98e0615a1
ArshpreetSinghGill <162580817+ArshpreetSinghGill@users.noreply.github.com>
2024-10-25 11:26:09 +0530

    Created YellowBird.java
    child class of bird

---
+++ core/src/main/java/com/AngryHomies/AngryBirds/YellowBird.java
@@ -0,0 +1,9 @@
+package com.AngryHomies.AngryBirds;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class YellowBird extends Bird {
+    public YellowBird(Texture texture, float x, float y, float width, float height) {
+        super(texture, x, y, width, height);
+    }
+}
