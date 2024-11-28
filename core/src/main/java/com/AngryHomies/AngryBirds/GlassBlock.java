fcac68104889174139bf1b220886a9201d8e80d1
ArshpreetSinghGill <162580817+ArshpreetSinghGill@users.noreply.github.com>
2024-11-29 00:07:48 +0530

    Delete core/src/main/java/com/AngryHomies/AngryBirds/GlassBlock.java
    Deleted and moved

--- core/src/main/java/com/AngryHomies/AngryBirds/GlassBlock.java
+++
@@ -1,9 +0,0 @@
-package com.AngryHomies.AngryBirds;
-
-import com.badlogic.gdx.graphics.Texture;
-
-public class GlassBlock extends Block {
-    public GlassBlock(Texture texture, float x, float y, float width, float height) {
-        super(texture, x, y, width, height);
-    }
-}
