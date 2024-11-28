2fd05cebee235ca21543169c1d2b9f043475a526
kushiiitd05 <162983932+kushiiitd05@users.noreply.github.com>
2024-11-29 00:10:47 +0530

    Update SerializableBird.java
    code for serlizabling bird and its serilsable data as all in bird is not serialisable

--- core/src/main/java/com/AngryHomies/AngryBirds/Serializables/SerializableBird.java
+++ core/src/main/java/com/AngryHomies/AngryBirds/Serializables/SerializableBird.java
@@ -1,1 +1,82 @@
+package com.AngryHomies.AngryBirds.Serializables;
+
+import com.AngryHomies.AngryBirds.entities.bird.Bird;
+
+import java.io.Serializable;
+
+public class SerializableBird implements Serializable {
+
+
+    private String id;
+    private float x,y;
+    private float width;
+    private float height;
+    private float density;
+    private float diameter;
+    private boolean isLaunched;
+    private float launchTime;
+    private float attackPower;
+    private SerializableBody bodyState;
+
+    public SerializableBird(Bird bird){
+        this.id = bird.getId();
+        this.x = bird.getX();
+        this.y = bird.getY();
+        this.width = bird.getWidth();
+        this.height = bird.getHeight();
+        this.density = bird.getDensity();
+        this.diameter = bird.getDiameter();
+        this.isLaunched = bird.isLaunched();
+        this.launchTime = bird.getLaunchTime();
+        this.attackPower = bird.getAttackPower();
+        if (bird.getBody() != null) {
+            this.bodyState = new SerializableBody(bird.getBody());
+        }
+    }
+
+    public float getDiameter() {
+        return diameter;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public float getAttackPower() {
+        return height;
+    }
+
+    public SerializableBody getBodyState() {
+        return bodyState;
+    }
+
+    public float getDensity() {
+        return density;
+    }
+
+    public boolean isLaunched() {
+        return isLaunched;
+    }
+
+    public float getLaunchTime() {
+        return launchTime;
+    }
+
+}
 
