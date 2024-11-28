bcaf3d28b20e7a76dc89583f69d7f106a9a82d08
ArshpreetSinghGill <162580817+ArshpreetSinghGill@users.noreply.github.com>
2024-11-29 00:11:15 +0530

    Delete core/src/main/java/com/AngryHomies/AngryBirds/StoneBlock.java
    Deleted and moved

--- core/src/main/java/com/AngryHomies/AngryBirds/StoneBlock.java
+++
@@ -1,9 +0,0 @@
-package com.AngryHomies.AngryBirds;
-
-import com.badlogic.gdx.graphics.Texture;
-
-public class StoneBlock extends Block {
-    public StoneBlock(Texture texture, float x, float y, float width, float height) {
-        super(texture, x, y, width, height);
-    }
-}
