ca1b506af98dd7136f755197e5d1d90c1b5b4206
kushiiitd05 <162983932+kushiiitd05@users.noreply.github.com>
2024-11-29 00:31:24 +0530

    Update BigPig.java
    updated it wit overloaded constructor

--- core/src/main/java/com/AngryHomies/AngryBirds/entities/pig/BigPig.java
+++ core/src/main/java/com/AngryHomies/AngryBirds/entities/pig/BigPig.java
@@ -1,1 +1,21 @@
+package com.AngryHomies.AngryBirds.entities.pig;
+
+import com.AngryHomies.AngryBirds.Serializables.SerializablePig;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class BigPig extends Pig {
+
+    public static final String id = "big_pig";
+    public BigPig(Texture texture,String id, float x, float y, float width, float height,float density ,float diameter,float hth,World world){
+        super(texture, id, x, y, width, height,density, diameter ,hth,world);
+    }
+
+    public BigPig(Texture texture, SerializablePig serializablePig, World world){
+        super(texture, serializablePig, world);
+    }
+
+    public BigPig(){
+    }
+}
 
