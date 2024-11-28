405d312011a431499c2649c845c336a38568c56a
ArshpreetSinghGill <162580817+ArshpreetSinghGill@users.noreply.github.com>
2024-11-29 00:34:06 +0530

    Update WoodBlock.java
    created a child class of Block class

--- core/src/main/java/com/AngryHomies/AngryBirds/entities/block/WoodBlock.java
+++ core/src/main/java/com/AngryHomies/AngryBirds/entities/block/WoodBlock.java
@@ -1,1 +1,31 @@
+package com.AngryHomies.AngryBirds.entities.block;
+
+import com.AngryHomies.AngryBirds.Serializables.SerializableBlock;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class WoodBlock extends Block {
+
+    public static final String id = "wooden_block";
+
+    public WoodBlock(Texture texture,String id, float x, float y, float width, float height,float density , float hth,World world) {
+        super(texture,id,  x, y, width, height,density,hth,world);
+    }
+
+    public WoodBlock(Texture texture, SerializableBlock serializableBlock, World world) {
+        super(
+            serializableBlock.getOrientation().equals("standing")
+                ? new Texture(Gdx.files.internal("images/standing_woodblock.png"))
+                : new Texture(Gdx.files.internal("images/block.png")),
+            serializableBlock,
+            world
+        );
+
+    }
+
+    public WoodBlock() {
+
+    }
+}
 
