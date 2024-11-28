d5608534d33927f155e89722613d3d61cd3cbb28
kushiiitd05 <162983932+kushiiitd05@users.noreply.github.com>
2024-11-29 00:30:42 +0530

    Update KingPig.java
    updated added new constructor overloaded for level state and serialization

--- core/src/main/java/com/AngryHomies/AngryBirds/entities/pig/KingPig.java
+++ core/src/main/java/com/AngryHomies/AngryBirds/entities/pig/KingPig.java
@@ -1,1 +1,19 @@
+package com.AngryHomies.AngryBirds.entities.pig;
+
+import com.AngryHomies.AngryBirds.Serializables.SerializablePig;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class KingPig extends Pig {
+
+    public static final String id = "king_pig";
+
+    public KingPig(Texture texture,String id, float x, float y, float width, float height,float density,float diameter , float hth,World world) {
+        super(texture,id, x, y, width, height,density,diameter,hth,world);
+    }
+
+    public KingPig(Texture texture, SerializablePig serializablePig, World world){
+        super(texture, serializablePig, world);
+    }
+}
 
