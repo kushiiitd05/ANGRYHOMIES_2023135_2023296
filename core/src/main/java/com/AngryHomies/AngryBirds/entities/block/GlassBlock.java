c77e3f19b9bf3e2ca1c8b6d0605813fe5e180a5c
ArshpreetSinghGill <162580817+ArshpreetSinghGill@users.noreply.github.com>
2024-11-29 00:31:33 +0530

    Update GlassBlock.java
    created a child class of Block

--- core/src/main/java/com/AngryHomies/AngryBirds/entities/block/GlassBlock.java
+++ core/src/main/java/com/AngryHomies/AngryBirds/entities/block/GlassBlock.java
@@ -1,1 +1,25 @@
+package com.AngryHomies.AngryBirds.entities.block;
+
+import com.AngryHomies.AngryBirds.Serializables.SerializableBlock;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class GlassBlock extends Block {
+    public static final String id = "glass_block";
+
+    public GlassBlock(Texture texture,String id, float x, float y, float width, float height,float density, float hth,World world) {
+        super(texture,id,  x, y, width, height,density,hth,world);
+    }
+
+    public GlassBlock(Texture texture, SerializableBlock serializableBlock, World world) {
+        super(
+            serializableBlock.getOrientation().equals("standing")
+                ? new Texture(Gdx.files.internal("images/glass_block.png"))
+                : new Texture(Gdx.files.internal("images/sp_glassblock.png")),
+            serializableBlock,
+            world
+        );
+    }
+}
 
