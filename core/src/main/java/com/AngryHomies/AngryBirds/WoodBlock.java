ac60a140dc5b2c158fd415d184e8f4b551aecc64
ArshpreetSinghGill <162580817+ArshpreetSinghGill@users.noreply.github.com>
2024-11-29 00:11:47 +0530

    Delete core/src/main/java/com/AngryHomies/AngryBirds/WoodBlock.java
    Deleted and moved

--- core/src/main/java/com/AngryHomies/AngryBirds/WoodBlock.java
+++
@@ -1,9 +0,0 @@
-package com.AngryHomies.AngryBirds;
-
-import com.badlogic.gdx.graphics.Texture;
-
-public class WoodBlock extends Block {
-    public WoodBlock(Texture texture, float x, float y, float width, float height) {
-        super(texture, x, y, width, height);
-    }
-}
