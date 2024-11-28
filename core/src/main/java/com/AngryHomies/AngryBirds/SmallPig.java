f7dcc827b40a6d57c505bef4aa6c893b9f6d7b7e
ArshpreetSinghGill <162580817+ArshpreetSinghGill@users.noreply.github.com>
2024-11-29 00:11:03 +0530

    Delete core/src/main/java/com/AngryHomies/AngryBirds/SmallPig.java
    Deleted and moved

--- core/src/main/java/com/AngryHomies/AngryBirds/SmallPig.java
+++
@@ -1,9 +0,0 @@
-package com.AngryHomies.AngryBirds;
-
-import com.badlogic.gdx.graphics.Texture;
-
-public class SmallPig extends Pig {
-    public SmallPig(Texture texture, float x, float y, float width, float height) {
-        super(texture, x, y, width, height);
-    }
-}
