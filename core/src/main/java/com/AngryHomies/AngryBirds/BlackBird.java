be4cce4d1883125ece776572f89e101a2cdb7cf0
ArshpreetSinghGill <162580817+ArshpreetSinghGill@users.noreply.github.com>
2024-11-29 00:07:00 +0530

    Delete core/src/main/java/com/AngryHomies/AngryBirds/BlackBird.java
    Deleted and moved

--- core/src/main/java/com/AngryHomies/AngryBirds/BlackBird.java
+++
@@ -1,9 +0,0 @@
-package com.AngryHomies.AngryBirds;
-
-import com.badlogic.gdx.graphics.Texture;
-
-public class BlackBird extends Bird {
-    public BlackBird(Texture texture, float x, float y, float width, float height) {
-        super(texture, x, y, width, height);
-    }
-}
