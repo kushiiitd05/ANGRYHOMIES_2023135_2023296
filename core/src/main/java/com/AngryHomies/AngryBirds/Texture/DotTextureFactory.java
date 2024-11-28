d25f3c5f2ddb3f83921201b6e989c1f1cc9f6cd2
kushiiitd05 <162983932+kushiiitd05@users.noreply.github.com>
2024-11-29 00:07:26 +0530

    Update DotTextureFactory.java

--- core/src/main/java/com/AngryHomies/AngryBirds/Texture/DotTextureFactory.java
+++ core/src/main/java/com/AngryHomies/AngryBirds/Texture/DotTextureFactory.java
@@ -1,1 +1,18 @@
+package com.AngryHomies.AngryBirds.Texture;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+
+public class DotTextureFactory implements TextureFactory {
+    @Override
+    public Texture createSlingShotTexture() {
+        Pixmap pixmap = new Pixmap(8, 8, Pixmap.Format.RGBA8888);
+        pixmap.setColor(Color.WHITE);
+        pixmap.fillCircle(4, 4, 4);
+        Texture dotTexture = new Texture(pixmap);
+        pixmap.dispose();
+        return dotTexture;
+    }
+}
 
