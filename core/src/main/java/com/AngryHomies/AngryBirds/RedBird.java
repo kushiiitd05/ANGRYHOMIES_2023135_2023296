f7715dedbd8289ee943e62a7354eb075c6e1cc7c
ArshpreetSinghGill <162580817+ArshpreetSinghGill@users.noreply.github.com>
2024-11-29 00:10:23 +0530

    Delete core/src/main/java/com/AngryHomies/AngryBirds/RedBird.java
    Deleted and moved

--- core/src/main/java/com/AngryHomies/AngryBirds/RedBird.java
+++
@@ -1,9 +0,0 @@
-package com.AngryHomies.AngryBirds;
-
-import com.badlogic.gdx.graphics.Texture;
-
-public class RedBird extends Bird {
-    public RedBird(Texture texture, float x, float y, float width, float height) {
-        super(texture, x, y, width, height);
-    }
-}
