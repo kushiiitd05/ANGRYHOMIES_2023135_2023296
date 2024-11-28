b7c4cbc8f9e9077f01a2f2bd1f2221c9feba0a90
ArshpreetSinghGill <162580817+ArshpreetSinghGill@users.noreply.github.com>
2024-11-29 00:27:32 +0530

    Update RedBird.java
    created red bird child of bird class with its special ability

--- core/src/main/java/com/AngryHomies/AngryBirds/entities/bird/RedBird.java
+++ core/src/main/java/com/AngryHomies/AngryBirds/entities/bird/RedBird.java
@@ -1,1 +1,37 @@
+package com.AngryHomies.AngryBirds.entities.bird;
+
+import com.AngryHomies.AngryBirds.Serializables.SerializableBird;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class RedBird extends Bird {
+
+    public static final String id = "red_bird";
+
+    public RedBird(Texture texture,String id, float x, float y, float width, float height,float density ,float diameter, float atk,World world) {
+        super(texture,id, x, y, width, height,density,diameter,atk,world);
+    }
+
+    public RedBird(Texture texture, SerializableBird serializableBird, World world){
+        super(texture, serializableBird, world);
+    }
+
+    public RedBird() {
+        super();
+    }
+    @Override
+    public void onSpecialAbility() {
+        if (abilityUsed || body.getLinearVelocity().len() < 0.1f) return;
+        abilityUsed = true;
+        Sound Sound =Gdx.audio.newSound(Gdx.files.internal("audio/red_attack.mp3"));
+        Sound.play(.5f);
+        Texture specialAbilityTexture = new Texture("images/red_attack.png");
+        this.textureRegion = new TextureRegion(specialAbilityTexture);
+        attackPower += 5;
+        System.out.println("RedBird ability triggered: Attack power increased to " + attackPower);
+    }
+}
 
