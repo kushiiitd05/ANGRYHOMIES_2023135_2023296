5df08365b3fe63415971bed6c15ad1042435b8c1
ArshpreetSinghGill <162580817+ArshpreetSinghGill@users.noreply.github.com>
2024-11-29 00:08:23 +0530

    Delete core/src/main/java/com/AngryHomies/AngryBirds/KingPig.java
    Deleted and moved

--- core/src/main/java/com/AngryHomies/AngryBirds/KingPig.java
+++
@@ -1,9 +0,0 @@
-package com.AngryHomies.AngryBirds;
-
-import com.badlogic.gdx.graphics.Texture;
-
-public class KingPig extends Pig {
-    public KingPig(Texture texture, float x, float y, float width, float height) {
-        super(texture, x, y, width, height);
-    }
-}
